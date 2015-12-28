54ebb7da644358ddbb98363a33927cb6e503858e
Yizheng Shen <tinirlove@x-10-104-103-252.uofm-secure.wireless.umn.edu>
2015-12-28 17:34:39 -0600

    add React w/ Webpack Electron, HTML5 boilerplate

---
+++ React Boilerplate/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './jsx/App.jsx';
+import './style/app.css';
+
+ReactDOM.render(<App />, document.getElementById('app'));
