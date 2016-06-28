9988d95bf963eb512de7e1be2b2824b5bb84fb24
Yizheng Shen <tinirlove@x-134-84-1-22.vpn.umn.edu>
2016-06-28 16:16:44 +0800

    some update

--- React Boilerplate/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './jsx/App.jsx';
-import './style/app.css';
-
-ReactDOM.render(<App />, document.getElementById('app'));
